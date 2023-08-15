7494aa368fb4c0e35c745fbf4ce1c030489152e9
smmartprogrammer <partabrajp@gmail.com>
2023-08-15 12:53:00 +0500

    changes on pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,7 @@
-import Footer from '@/components/Footer'
-import Hero from '@/components/Hero'
-import Newsletter from '@/components/Newsletter'
-import Promotion from '@/components/Promotion'
-// import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <Hero />
-      <Promotion />
-      <Newsletter />
-      <Footer /> */}
     </main>
   )
 }
