5952d79f1e40f3c77deccd6940cafdaa3027d7f0
smmartprogrammer <partabrajp@gmail.com>
2023-08-15 23:06:16 +0500

    commit out

--- components/Types.tsx
+++ components/Types.tsx
@@ -13,39 +13,39 @@ export const NavbarArray: Array<NavbarItemTypes> = [
 ];
 
 
-export interface oneProductType {
-  _id: string;
-  quantity:number,
-  _rev: String,
-  _type:string,
-  productName:string,
-  _creditAt:string,
-  _id:string,
-  _updateAt: string,
-  // image:Array<imageType,>
-  name: string;
-  title:string;
-  price: number;
-  // img: image | string | StaticImageData;
-  category: string;
-  gender?: string;
-  href?: string;
-  item: any
-}
+// export interface oneProductType {
+//   _id: string;
+//   quantity:number,
+//   _rev: String,
+//   _type:string,
+//   productName:string,
+//   _creditAt:string,
+//   _id:string,
+//   _updateAt: string,
+//   // image:Array<imageType,>
+//   name: string;
+//   title:string;
+//   price: number;
+//   // img: image | string | StaticImageData;
+//   category: string;
+//   gender?: string;
+//   href?: string;
+//   item: any
+// }
 
 
 
-export interface Product {
-  id: string;
-  name: string;
-  title:string;
-  price: number;
-  // img: image | string | StaticImageData;
-  category: string;
-  gender?: string;
-  href?: string;
-  item: any
-}
+// export interface Product {
+//   id: string;
+//   name: string;
+//   title:string;
+//   price: number;
+//   // img: image | string | StaticImageData;
+//   category: string;
+//   gender?: string;
+//   href?: string;
+//   item: any
+// }
 
 
-export const ProductsList = [];
+// export const ProductsList = [];
