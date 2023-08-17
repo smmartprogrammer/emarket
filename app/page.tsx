4f299ea787ef7d67738244bfff872d0af5c004b4
smmartprogrammer <partabrajp@gmail.com>
2023-08-18 01:39:09 +0500

    working on product carosal

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import { oneProductType, responseType } from "@/components/Types";
 
 async function fetchAllProductsData() {
   let res = await fetch(`https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-06-07/data/query/production?query=*[_type == "products"]`, {
+
     next: {
       revalidate: 60
     }
@@ -24,6 +25,7 @@ async function fetchAllProductsData() {
 export default async function Home() {
   let { result }: responseType = await fetchAllProductsData();  
   
+  console.log(result);
   
   return (
 
--- components/ProductCarrosal.tsx
+++ components/ProductCarrosal.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { FC } from 'react'
+import { oneProductType } from './Types'
 
-
-const ProductCarrosal = ({ProductData}: any) => {
+const ProductCarrosal: FC<{ProductData: Array<oneProductType>}> = ({ProductData}) => {
   return (
     <div>{ProductData[0].productName }</div>
   )
