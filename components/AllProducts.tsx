73c6c91ce7eeca6fb1e51da12ac459c6483846a2
smmartprogrammer <partabrajp@gmail.com>
2023-08-21 01:46:45 +0500

    working on productall page

--- components/AllProducts.tsx
+++ components/AllProducts.tsx
@@ -1,7 +1,9 @@
 "use client"
-
 import { Component } from "react";
 import { oneProductType } from "./Types";
+import BASE_PATH_FORAPI from "./BasePath";
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Card from "./Card";
 
 
 interface propsType {
@@ -9,14 +11,57 @@ interface propsType {
 }
 
 export default class AllProductsCompo extends Component<{ ProductData: propsType }> {
-    getData = () => {
-        console.log(this.props.ProductData.productArray[0].price);
-        
+    start: number = 10;
+    end: number = 20;
+    state: any = {
+        items: [...this.props.ProductData.productArray],
+        hasMore: true,
     }
-    
+
+    fetchDataFromApiGradually = async (start: number, end: number) => {
+        const res = await fetch(`${BASE_PATH_FORAPI}/api/products?start${start}&end=${end}`)
+        const dataToCheckAndSend = await res.json()
+        if (dataToCheckAndSend.productArray === "Not Found") {
+            this.setState({ hasMore: false })
+        }
+        return dataToCheckAndSend
+    }
+
+    getData = async () => {
+        let allTogether = await this.fetchDataFromApiGradually(this.start, this.end);
+        if (allTogether.productArray !== "Not Found") {
+            this.setState({
+                items: this.state.items.concat(allTogether.productArray)
+            })
+                
+        } else {
+            this.setState({
+                hasMore: false
+            })
+        }
+        this.start = this.start + 10;
+        this.end = this.end + 10;
+    }
+
     render() {
         return (
-            <div onClick={this.getData}> {this.props.ProductData.productArray[0].price} </div>
+            <div onClick={() => this.getData}>
+                <InfiniteScroll
+                    dataLength={this.state.items.length} //This is important field to render the next data
+                    next={this.getData}
+                    hasMore={this.state.hasMore}
+                    loader={<h4>Loading...</h4>}
+                    endMessage={
+                        <p style={{ textAlign: 'center' }}>
+                            <b>Yay! You have seen it all</b>
+                        </p>
+                    }
+                    className="content-center justify-center py-10 grid grid-col-1 md:grid-col-2 lg:grid-col-3 gap-4 "
+                >{this.state.items.map((item: oneProductType, index: number) => (
+                    <Card key={index} singleProduct={item} />
+                ))}
+                </InfiniteScroll>
+            </div>
         )
     }
 }
--- components/Card.tsx
+++ components/Card.tsx
@@ -10,23 +10,22 @@ import Link from 'next/link'
 
 const Card: FC<{ singleProduct: oneProductType }> = ({ singleProduct }) => {
     return (
-<Link    href={`/cataloge${singleProduct.slug.current}`}    >
-        <div className='max-w-sm min-w-[24rem] space-y-3 select-none hover:scale-110 duration-300'>
-            <div className='relative w-full'>
-                <div className='absolute inset-0 z-10' />
-                <Image width={1000} height={1000} src={urlForImage(singleProduct.image[0]).width(1000).height(1000).url()} alt='imagesfrom sanity' />
+        <Link href={`/cataloge${singleProduct.slug.current}`}    >
+            <div className='max-w-sm min-w-[24rem] space-y-3 select-none hover:scale-110 duration-300'>
+                <div className='relative w-full'>
+                    <div className='absolute inset-0 z-10' />
+                    <Image width={1000} height={1000} src={urlForImage(singleProduct.image[0]).width(1000).height(1000).url()} alt='imagesfrom sanity' />
+                </div>
 
+                <h6 className="font-bold text-lg mt-3 text-[#5A5A5A] py-1 ">
+                    {singleProduct.productName}
+                </h6>
+                <p className="font-bold text-base text-[#888888] py-1 ">
+                    {singleProduct.price}
+                </p>
             </div>
-
-            <h6 className="font-bold text-lg mt-3 text-[#5A5A5A] py-1 ">
-                {singleProduct.productName}
-            </h6>
-            <p className="font-bold text-base text-[#888888] py-1 ">
-                {singleProduct.price}
-            </p>
-        </div>
         </Link>
-        
+
     )
 }
 
